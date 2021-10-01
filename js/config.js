ba16f69518dfa10fea13e2b7121d742b00dc9376
paurush batish <paurushbatishfbd@gmail.com>
2021-10-01 13:14:21 +0530

    new js for pinput fields

--- js/config.js
+++ js/config.js
@@ -20,3 +20,32 @@ window._skel_config = {
 window._skel_panels_config = {
 	preset: 'standard'
 };
+
+var $count = 0;
+
+var $eb = $('.edit-button');
+var $ei = $('.editable-input');
+var $ec = $('.editable-cell');
+
+//Editable input fields
+$eb.on('click', function() {
+  $count++
+  if ($count < 2) {
+    $ei.prop('readonly', false).focus();
+    $ei.prop('placeholder','');
+    $ei.val('');
+    $(this).addClass('hide');
+    $ec.addClass('editing');
+  } else {
+    $ei.prop('readonly', false).focus();
+    $eb.addClass('hide');
+    $ec.addClass('editing');
+  }
+});
+
+
+$ei.on('blur', function() {
+  $eb.removeClass('hide');
+  $ei.prop('readonly', true);
+  $ec.removeClass('editing');
+});
